4f4bdd56290bb086951daae1b78c610450313503
Stan Verhoeckx <stion.verhoeckx@gmail.com>
2013-06-06 12:57:26 +0200

    Changed CSS

--- src/tictactoe/view.clj
+++ src/tictactoe/view.clj
@@ -1,46 +1,46 @@
-(ns galgje.view
+(ns tictactoe.view
   (:use hiccup.form
         [hiccup.def :only [defhtml]]
         [hiccup.element :only [link-to]]
         [hiccup.page :only [html5 include-css]])
-  (:require [galgje.model :as model]))
+  (:require [tictactoe.model :as model]))
 
 (defhtml layout [& content]
   (html5
    [:head
-    [:title "Welcome to galgje-luminus"]
-    (include-css "/css/galgje.css")]
+    [:title "Welcome to ticsdfsdfdfstactoe-luminus"]
+    (include-css "/css/tictactoe.css")]
    [:body [:div#wrapper content]]))
 
-(defn cell-html [rownum colnum cell with-submit?]
-  [:td
-   [:input {:name (str "b" rownum colnum)
+(defn cell-html [rownum colnum cell with-submit?] 
+  [:td 
+   [:input {:name (str "b" rownum colnum) 
             :value (str cell)
-            :type (if with-submit?
-                    "submit"
+            :type (if with-submit? 
+                    "submit" 
                     "button")}]])
-
+  
 (defn row-html [rownum row with-submit?]
   [:tr (map-indexed (fn [colnum cell]
                       (cell-html rownum colnum cell with-submit?))
                     row)])
-
+  
 (defn board-html [board with-submit?]
   (form-to [:post "/"]
-           [:table
+           [:table 
             (map-indexed (fn [rownum row]
-                           (row-html rownum row with-submit?))
+                           (row-html rownum row with-submit?)) 
                          board)]))
 
 (defn play-screen []
   (layout
-    [:div
-     [:p "Player " (model/get-player) ", choose a word!"]
+    [:div 
+     [:p "Player " (model/get-player) ", it is your turn!"]
      (board-html (model/get-board) true)]))
 
 (defn winner-screen [winner]
   (layout
-    [:div
+    [:div 
    [:p "The winner is: " winner]
    (board-html (model/get-board) false)
    (link-to "/" "Reset")]))
@@ -51,4 +51,4 @@
      [:p "It's a draw!"]
      (board-html (model/get-board) false)
      (link-to "/" "Reset")]))
-
+  
